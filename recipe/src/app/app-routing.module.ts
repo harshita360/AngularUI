4207283df78dff5bc4d105184855810d0eac7ff4
harshita360 <harshita360@gmail.com>
2023-12-18 17:52:17 +0530

    Adding Route guards

--- recipe/src/app/app-routing.module.ts
+++ recipe/src/app/app-routing.module.ts
@@ -7,10 +7,11 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {path:'',redirectTo:'/recipes', pathMatch:'full'},
-  {path:'recipes', component:RecipesComponent, children:[
+  {path:'recipes', component:RecipesComponent, canActivate: [AuthGuard],children:[
     {path:'', component:RecipeStartComponent },//for the route just /recipe
     {path: 'new', component:RecipeEditComponent},
     {path: ':id', component:RecipeDetailComponent,resolve:[RecipeResolverService] },
